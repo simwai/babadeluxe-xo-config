30f797efedb464116c817f6aeb2a46ef26cc8807
simwai <16225108+simwai@users.noreply.github.com>
2025-09-16 11:41:13 +0200

    chore: :hammer: Moved the packaging testing from test-script.sh to a npm script

--- test-package.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf ./package
-rm -rf ./babadeluxe-xo-config-*.tgz
-npm pack @babadeluxe/xo-config
-tar -xvf ./babadeluxe-xo-config-*.tgz
-rm -rf ./babadeluxe-xo-config-*.tgz
-rm -rf node_modules
-echo Please run npm i from batch now
