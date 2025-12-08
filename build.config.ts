867c87379f3a431e7887cc052af19f5b4aacc8bd
simwai <16225108+simwai@users.noreply.github.com>
2025-12-08 12:25:40 +0100

    revert: :rewind: Removed eslint-plugin-no-explicit-undefined dependency and configuration

--- build.config.ts
+++ build.config.ts
@@ -3,7 +3,6 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   entries: ['./src/index'],
   declaration: 'compatible', // generates .d.ts, .d.mts AND .d.cts
-  externals: ['eslint-plugin-no-explicit-undefined'],
   rollup: {
     emitCJS: true,
   },
