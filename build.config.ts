f1c6c69ce5153a746d2639c8d29fdd6d8bf0f949
simwai <16225108+simwai@users.noreply.github.com>
2026-04-04 08:44:12 +0200

    fix: :bug: Fixed publish process

--- build.config.ts
+++ build.config.ts
@@ -1,7 +1,6 @@
 import { copyFile, mkdir } from 'node:fs/promises'
 import { join } from 'node:path'
 import { defineBuildConfig } from 'unbuild'
-import { globby } from 'globby'
 
 export default defineBuildConfig({
   entries: ['./xo.config.ts'],
@@ -10,18 +9,6 @@ export default defineBuildConfig({
     emitCJS: true,
     esbuild: {
       treeShaking: true,
-    },
-  },
-  hooks: {
-    async 'build:done'(_ctx) {
-      const mdFiles = await globby('*.md', { ignore: ['node_modules', 'dist'] })
-      await mkdir('dist', { recursive: true })
-      await Promise.all(
-        mdFiles.map(async (file) => {
-          await copyFile(file, join('dist', file))
-        })
-      )
-      console.log(`✅ Copied ${mdFiles.length} markdown file(s) to dist/`)
     },
   },
 })
--- publish.ts
+++ publish.ts
@@ -1,10 +1,23 @@
 import { execSync } from 'node:child_process'
 import process from 'node:process'
+import fs from 'node:fs'
+import path from 'node:path'
+import * as dotenv from 'dotenv'
 
 const bump = process.argv[2] ?? 'patch'
 
-if (!process.env['NPM_TOKEN']) {
-  throw new Error('Missing NPM_TOKEN in .env or environment')
+const envLocalPath = path.resolve('.', './.env.local')
+const envPath = path.resolve('.', './.env')
+
+const isEnvLocalExisting = fs.existsSync(envLocalPath)
+const isEnvExisting = fs.existsSync(envPath)
+
+if (isEnvLocalExisting) {
+  dotenv.config({ path: envLocalPath })
+} else if (isEnvExisting) {
+  dotenv.config({ path: envPath })
+} else {
+  throw new Error('No env file found. Pls create one.')
 }
 
 execSync('npm run build', { stdio: 'inherit', env: process.env })
